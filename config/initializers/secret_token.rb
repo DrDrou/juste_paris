0a6402c72f5def1944a24f99ceebfe791dad79bf
Gougui <drouetg@gmail.com>
2013-07-21 12:05:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JusteParis::Application.config.secret_token = '4642e54624fe39f6ea6c1f6b2bfd15d988f042b2c116a055135f6a9eeee47aff2768a28da98a12f88d8ba6fc0b1bc12778f7dc7a1c83ec86413c812b9448f91a'
